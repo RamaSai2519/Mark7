41111993cf11d918b679f405565e14d5cf0572ee
RamaSai2519 <rchandik@gitam.in>
2024-04-26 21:18:07 +0530

    Final commit

--- mistake.py
+++ mistake.py
@@ -11,40 +11,26 @@ db = client["test"]
 collection = db["calls"]
 users = db["users"]
 
-{
-    "_id": {"$oid": "662b51f5a88f80756faa7a0b"},
-    "callId": "a441a92e-9fbb-4458-b4bf-77e72e31c794",
-    "status": "successfull",
-    "initiatedTime": {"$date": {"$numberLong": "1714115061370"}},
-    "duration": "0:04:10",
-    "transferDuration": "00:03:47",
-    "recording_url": "https://sr.knowlarity.com/vr/fetchsound/?callid%3Da441a92e-9fbb-4458-b4bf-77e72e31c794",
-    "failedReason": "",
-    "expert": {"$oid": "66046a3d42f04a057fa21034"},
-    "user": {"$oid": "66178da6a6eca419e5884569"},
-    "__v": {"$numberInt": "0"},
-}
-
 document = {
-    "callId": "410c6294-f409-40c7-bd2c-61c125ffd9a6",
+    "callId": "3975dd83-50dc-4be3-81be-1c8b9be31ee1",
     "status": "successfull",
-    "initiatedTime": datetime.datetime(2024, 4, 26, 4, 24, 13, 926000),
-    "duration": "0:16:45",
-    "transferDuration": "00:16:37",
-    "recording_url": "https://sr.knowlarity.com/vr/fetchsound/?callid%3D410c6294-f409-40c7-bd2c-61c125ffd9a6",
+    "initiatedTime": datetime.datetime(2024, 4, 26, 4, 0, 26, 474000),
+    "duration": "0:23:03",
+    "transferDuration": "00:22:55",
+    "recording_url": "https://sr.knowlarity.com/vr/fetchsound/?callid%3D3975dd83-50dc-4be3-81be-1c8b9be31ee1",
     "failedReason": "",
     "expert": ObjectId("6604665042f04a057fa20ddc"),
     "user": ObjectId("661526bc6159273a018e7b89"),
     "__v": 0,
 }
 
-collection.insert_one(document)
-print("Document inserted successfully.")
+# collection.insert_one(document)
+# print("Document inserted successfully.")
 
-sleep(5)
+# sleep(5)
 
-collection.delete_one({"callId": "410c6294-f409-40c7-bd2c-61c125ffd9a6"})
-print("Document deleted successfully.")
+# collection.delete_one({"callId": "3975dd83-50dc-4be3-81be-1c8b9be31ee1"})
+# print("Document deleted successfully.")
 
-# calls = list((collection.find({"callId": "410c6294-f409-40c7-bd2c-61c125ffd9a6"})))
-# pprint.pprint(calls)
+calls = list((collection.find({"callId": "3975dd83-50dc-4be3-81be-1c8b9be31ee1"})))
+pprint.pprint(calls)
