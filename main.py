cfed37c24d525eb6a0a847945e64f7a5805464d9
Rama Sathya Sai <rchandik@gitam.in>
2024-05-11 04:58:26 +0530

    chore: Process calls with duration greater than 5 minutes
    ```

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ def main():
     db_uri = "mongodb+srv://sukoon_user:Tcks8x7wblpLL9OA@cluster0.o7vywoz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     client = pymongo.MongoClient(db_uri)
     db = client.test
-
     calls_collection = db.calls
     user_document = None
     expert_document = None
@@ -22,14 +21,16 @@ def main():
                 user_document = db.users.find_one({"_id": call["user"]})
             if expert_document is None:
                 expert_document = db.experts.find_one({"_id": call["expert"]})
-            try:
-                user = user_document["name"]
-                expert = expert_document["name"]
-                process_call_data([call], user, expert, db, user_document)
-                updater()
-            except Exception as e:
-                error_message = f"An error occurred processing the call ({call.get('callId')}): {str(e)}"
-                notify(error_message)
+            
+            if call["duration"] >= "00:05:00":
+                try:
+                    user = user_document["name"]
+                    expert = expert_document["name"]
+                    process_call_data([call], user, expert, db, user_document)
+                    updater()
+                except Exception as e:
+                    error_message = f"An error occurred processing the call ({call.get('callId')}): {str(e)}"
+                    notify(error_message)
 
 
 if __name__ == "__main__":
--- main2.py
+++ main2.py
@@ -12,25 +12,26 @@ def main():
         successful_calls = list(db.calls.find({"status": "successfull"}))
 
         for call in successful_calls:
-            if "Conversation Score" not in call and call.get("recording_url") not in [
-                "None",
-                "",
-            ]:
-                f"Processing call: ({call.get('callId')})"
-                error_message = f"Processing call: ({call.get('callId')})"
-                notify(error_message)
-                try:
-                    user_document = db.users.find_one({"_id": call.get("user", "")})
-                    expert_document = db.experts.find_one(
-                        {"_id": call.get("expert", "")}
-                    )
-                    user = user_document["name"]
-                    expert = expert_document["name"]
-                    process_call_data([call], user, expert, db, user_document)
-                    updater()
-                except Exception as e:
-                    error_message = f"An error occurred processing the call ({call.get('callId')}): {str(e)}"
+            if call["duration"] >= "00:05:00":
+                if "Conversation Score" not in call and call.get("recording_url") not in [
+                    "None",
+                    "",
+                ]:
+                    f"Processing call: ({call.get('callId')})"
+                    error_message = f"Processing call: ({call.get('callId')})"
                     notify(error_message)
+                    try:
+                        user_document = db.users.find_one({"_id": call.get("user", "")})
+                        expert_document = db.experts.find_one(
+                            {"_id": call.get("expert", "")}
+                        )
+                        user = user_document["name"]
+                        expert = expert_document["name"]
+                        process_call_data([call], user, expert, db, user_document)
+                        updater()
+                    except Exception as e:
+                        error_message = f"An error occurred processing the call ({call.get('callId')}): {str(e)}"
+                        notify(error_message)
 
         time.sleep(3600)
 
