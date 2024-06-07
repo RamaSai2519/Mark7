0def52a06caa145cb0ef4cc0f6c8a15579063b60
Rama Sathya Sai <rchandik@gitam.in>
2024-06-07 17:26:23 +0530

    chore: Update main.py to use collections from config.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from process_call_data import process_call_data
 from score_updater import updater
 from notify import notify
 from Score_corrector import corrector
-from config import calls_collection, db
+from config import calls_collection, users_collection, experts_collection, db
 
 pipeline = [
     {
@@ -14,13 +14,12 @@ pipeline = [
 ]
 
 with calls_collection.watch(pipeline) as stream:
+    print("started...")
     for change in stream:
         call = change["fullDocument"]
-        if user_document is None:
-            user_document = db.users.find_one({"_id": call["user"]})
-        if expert_document is None:
-            expert_document = db.experts.find_one({"_id": call["expert"]})
-        duration = call.get("duration", "00:00:00")
+        user_document = users_collection.find_one({"_id": call["user"]})
+        expert_document = experts_collection.find_one({"_id": call["expert"]})
+        duration = call["duration"] if "duration" in call else "00:00:00"
         seconds = sum(
             int(x) * 60**i for i, x in enumerate(reversed(duration.split(":")))
         )
@@ -29,11 +28,13 @@ with calls_collection.watch(pipeline) as stream:
                 user = user_document["name"]
                 expert = expert_document["name"]
                 notify(
-                    f"Processing call {str(call.get('callId'))} between {user} and {expert}"
+                    f"Processing call {str(call["callId"])} between {user} and {expert}"
                 )
+                print(f"Processing call {str(call['callId'])} between {user} and {expert}")
                 process_call_data([call], user, expert, db, user_document)
                 corrector(call["callId"])
                 updater()
             except Exception as e:
                 error_message = f"An error occurred processing the call ({call.get('callId')}): {str(e)} on main loop"
                 notify(error_message)
+                print(error_message)
