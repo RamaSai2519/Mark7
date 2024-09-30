48fdf3eed25eba1b74ab89f768d3faaf980e855c
Rama Sathya Sai <rchandik@gitam.in>
2024-09-30 17:51:43 +0530

    Refactor score_updater.py to use dynamic expert_id and expert_number

--- score_updater.py
+++ score_updater.py
@@ -5,8 +5,8 @@ import json
 
 def updater(expert_id: str, expert_number: str) -> None:
     payload = json.dumps({
-        "expert_id": "6604675f42f04a057fa20e09",
-        "expert_number": "7596938218"
+        "expert_id": expert_id,
+        "expert_number": expert_number
     })
     headers = {'Content-Type': 'application/json'}
     response = requests.request("POST", url, headers=headers, data=payload)
